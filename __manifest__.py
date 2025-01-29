cccb9785e05979fc41798f2716a32f21ccf18b03
JuanJoven01 <juanjov95@hotmail.com>
2025-01-29 23:01:35 +0100

    fix(views): fix views
    add permissions and fix bugs to see views

--- __manifest__.py
+++ __manifest__.py
@@ -22,10 +22,10 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'security/ir.model.access.csv',
+        # 'views/views.xml',
         'views/exercises_views.xml',
-        'views/templates.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
