3a60142dae01601385cccb15638c9fa25084e716
JuanJoven01 <juanjov95@hotmail.com>
2025-02-02 23:15:10 +0100

    fix: exercises and focus area
    Change get endpoint type to http

--- controllers/exercises.py
+++ controllers/exercises.py
@@ -2,30 +2,38 @@ import logging
 from odoo import http
 from odoo.http import request, Response
 from .auth import JWTAuth
-from ._helpers import _success_response, _error_response
+from ._helpers import _http_success_response, _http_error_response
 from odoo.exceptions import AccessDenied
-# _logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 class ExercisesController(http.Controller):
 
-    @http.route('/api/easy_apps/gym/exercises', type='jsonrpc', auth="none", methods=['POST'])
+    @http.route('/api/easy_apps/gym/exercises', type='http', auth="none", methods=['GET'], csrf=False)
     def get_exercises(self, **kwargs):
         """Retrieve a list of exercises"""
         try:
-            jwt_body = JWTAuth.authenticate_request()  # Verify JWT authentication
-            if jwt_body['error']: return _error_response(jwt_body['error'], 401)
+            #  Authenticate user using JWT
+            user = JWTAuth.authenticate_request()
+
+            #  Check if authentication failed
+            if 'error' in user:
+                return _http_error_response(user['error'], 401)  # Unauthorized
+
+            #  Fetch all exercises
             exercises = request.env['easy_gym.exercises'].sudo().search([])
 
+            #  Format response data
             exercise_list = [{
-                'id': e.id, 
-                'name': e.name, 
+                'id': e.id,
+                'name': e.name,
                 'focus_area_id': e.focus_area_id.id if e.focus_area_id else None,
                 'image': f"data:image/png;base64,{e.image.decode('utf-8')}" if e.image else None
             } for e in exercises]
 
-            return _success_response(exercise_list, "Exercises retrieved successfully")
-        
+            return _http_success_response(exercise_list, "Exercises retrieved successfully", 200)
+
         except AccessDenied as e:
-            return _error_response(str(e), 401)  # Unauthorized
+            return _http_error_response(str(e), 401)  # Unauthorized
         except Exception as e:
-            return _error_response(str(e), 500)
+            _logger.error(f"Error retrieving exercises: {str(e)}")
+            return _http_error_response("Internal Server Error", 500)
--- controllers/focus_area.py
+++ controllers/focus_area.py
@@ -2,30 +2,37 @@ import logging
 from odoo import http
 from odoo.http import request, Response
 from .auth import JWTAuth
-from ._helpers import _success_response, _error_response
+from ._helpers import _http_error_response, _http_success_response
 from odoo.exceptions import AccessDenied
 
-# _logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 class FocusAreaController(http.Controller):
-
-    @http.route('/api/easy_apps/gym/focus_areas', type='jsonrpc', auth="none", methods=['POST'])
+    @http.route('/api/easy_apps/gym/focus_areas', type='http', auth="none", methods=['GET'], csrf=False)
     def get_focus_areas(self, **kwargs):
         """Retrieve a list of focus areas"""
         try:
-            JWTAuth.authenticate_request()  # Verify JWT authentication
+            #  Authenticate user using JWT
+            user = JWTAuth.authenticate_request()
 
+            #  Check if authentication failed
+            if 'error' in user:
+                return _http_error_response(user['error'], 401)  # Unauthorized
+
+            #  Fetch all focus areas
             focus_areas = request.env['easy_gym.focus_area'].sudo().search([])
 
+            #  Format response data
             focus_area_list = [{
                 'id': f.id,
                 'name': f.name,
                 'image': f"data:image/png;base64,{f.image.decode('utf-8')}" if f.image else None
             } for f in focus_areas]
 
-            return _success_response(focus_area_list, "Focus areas retrieved successfully")
+            return _http_success_response(focus_area_list, "Focus areas retrieved successfully", 200)
 
         except AccessDenied as e:
-            return _error_response(str(e), 401)  # Unauthorized
+            return _http_error_response(str(e), 401)  # Unauthorized
         except Exception as e:
-            return _error_response(str(e), 500)
+            _logger.error(f"Error retrieving focus areas: {str(e)}")
+            return _http_error_response("Internal Server Error", 500)
